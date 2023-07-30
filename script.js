347f0bc25bc3e9c676defbc5a7753dc92bb68247
Taylor016 <taylorr.016@gmail.com>
2023-07-30 17:19:26 -0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+// write code for container element
+
+//fetch using traditional syntax
+
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+response.json().then(function (data) {
+
+    for(let i=0; i < data.length; i++) {
+        let astronaut = data[i];
+        container.innerHTML += `
+        <div class="astronaut">
+          <div class="bio">
+             <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+             <ul>
+                  <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                  <li>Active: ${astronaut.active}</li>
+                  <li>Skills: ${astronaut.skills.join(", ")}</li>
+            </ul>
+          </div>
+         <img class="avatar" src="${astronaut.picture}">
+       </div>
+        `;
+
+    }
+});
+});
+
+
+});
